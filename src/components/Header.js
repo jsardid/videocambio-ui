77d5939e0f14bcb7e0fc54286c0bf67e5cdf1520
Jesus Suarez Ardid <jesussuarezardid@gmail.com>
2018-03-28 07:40:10 +1100

    Add home link to logo

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,13 @@
 import React from "react";
 import "./Header.css";
 import InputSearch from "./InputSearch";
+import { Link } from "react-router-dom";
 
 export const Header = () => (
   <header position="static">
-    <img src="logo.png" className="logo" alt="Video Cambio 100 Logo" />
+    <Link className="movie-link" to="">
+      <img src="logo.png" className="logo" alt="Video Cambio 100 Logo" />
+    </Link>
     <InputSearch />
   </header>
 );
