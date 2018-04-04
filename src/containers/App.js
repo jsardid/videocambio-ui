273616a6bffecee885aae2d36f1bd0da222de890
Jesus Suarez Ardid <jesussuarezardid@gmail.com>
2018-04-04 11:17:41 +1000

    Move routes to app.js

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,12 +1,27 @@
 import React from "react";
-import { Main } from "./../components/Main";
+import { Switch, Route } from "react-router-dom";
 import { Header } from "./../components/Header";
-import "./App.css";
+import { Home } from "./../pages/Home";
+import MovieDetailContainer from "./../pages/MovieDetailContainer";
+import { SearchResults } from "./../pages/SearchResults";
+import styled from "styled-components";
+
+const Content = styled.div`
+  margin-top: 120px !important;
+  background:  rgb(20, 22, 60);
+  background-attachment: fixed;
+`;
 
 const App = () => (
   <div>
     <Header />
-    <Main />
+    <Content>
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route exact path="/movies/:movieId" component={MovieDetailContainer} />
+        <Route exact path="/search" component={SearchResults} />
+      </Switch>
+    </Content>
   </div>
 );
 
